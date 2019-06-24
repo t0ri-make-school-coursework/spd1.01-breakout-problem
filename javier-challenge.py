190d9d5d78f7f75de50bd5c764189652603250f3
t0ri <victoriarosemurray@gmail.com>
2019-06-23 19:31:56 -0700

    coded implementations

--- javier-challenge.py
+++ javier-challenge.py
@@ -3,11 +3,25 @@ Javier's Challenge:
 Given a list of n numbers, determine if it contains any duplicate numbers.
 """
 
-# Whiteboard Implementation
-dict = dict()
-for elements in n:
-    if dict[element] == element:
-        return True
-    else:
-        dict[element] = element
-return False
+def check_duplicates(numbers):
+    numbers_dict = dict()
+    for number in numbers:
+        if number in numbers_dict:
+            return True
+        numbers_dict[number] = 1
+    return False
+
+
+def main():
+    test_cases = list()
+    test_cases.append((True, [3, 11, 5, 9, 7, 5])) # normal true
+    test_cases.append((False, [9, 2, 7, 15, 11, 4])) # normal false
+    test_cases.append((False, [4, 6, 13, -4, 8, 1 ])) # pos/neg duplicates
+    test_cases.append((True, [str, '', 2, '2', -0, float('inf'), '2'])) # edge cases etc
+
+    for expected, test_case in test_cases:
+        print('Test Case: ({}, {}) -- Result: {}'.format(test_case, expected, check_duplicates(test_case)))
+
+
+if __name__ == "__main__":
+    main()
--- tori-challenge.py
+++ tori-challenge.py
@@ -4,17 +4,33 @@ Find the middle item in a singly linked list,
 or two middle items if it contains an even number of nodes.
 """
 
-# Whiteboard Implementation
-iterator = 0
-length = self.size - 1
-mid = length // 2
+from linkedlist import LinkedList
 
-while node is not None:
-    if iterator == mid:
-        if length % 2 == 1:
-            return node.data
-        next_node = node.next
-        return (node.data, next_node.data)
+def get_middle(ll):
+    iterator = 0
+    length = ll.size - 1
+    node = ll.head # added this line
     
-    iterator += 1
-    node = node.next
+    while node is not None:
+        if iterator == length // 2:
+            if length % 2 == 0: # '== 1' to '== 0'
+                return node.data
+            next_node = node.next
+            return (node.data, next_node.data)
+        
+        iterator += 1
+        node = node.next
+
+def main():
+    linked_list_odd = LinkedList()
+    for item in ['A', 'B', 'C']:
+        linked_list_odd.append(item)
+    print('Odd List: {}, returns one: {}'.format(linked_list_odd, get_middle(linked_list_odd)))
+
+    linked_list_even = LinkedList()
+    for item in ['A', 'B', 'C', 'D']:
+        linked_list_even.append(item)
+    print('Even List: {}, returns two: {}'.format(linked_list_even, get_middle(linked_list_even)))
+
+if __name__ == "__main__":
+    main()
